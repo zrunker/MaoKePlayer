a1cb7b6b3797c62b9a2740db10f0b5d62dbc15d6
zrunker <1823132505@qq.com>
2022-06-10 11:28:36 +0800

    init Commit

---
+++ zffmpeglib/src/main/java/cc/zrunker/android/zffmpeglib/MaoKeMuxerUtil.java
@@ -0,0 +1,21 @@
+package cc.zrunker.android.zffmpeglib;
+
+/**
+ * FFmpeg4管理类
+ *
+ * @author 邹峰立
+ */
+public class MaoKeMuxerUtil {
+    static {
+        System.loadLibrary("muxer-util");
+    }
+
+    /**
+     * 格式转换
+     *
+     * @param inPath  输入文件路径
+     * @param outPath 输入文件路径
+     * @return boolean true-转换成功，false-转换失败
+     */
+    public native static boolean reMuxer(String inPath, String outPath);
+}
